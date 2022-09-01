3d795b14a7c025ecbd3a171b093fac4502a00f65
Alexandre Liscia <alexandre.liscia@orinoko.fr>
2022-09-01 14:29:01 +0200

    Implement player movement and rotation

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -1,12 +1,12 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class InputManager : MonoBehaviour
 {
+    public float verticalInput;
+    public float horizontalInput;
+
+    Vector2 _movementInput;
     PlayerControls _playerControls;
-    public Vector2 movementInput;
 
     void OnEnable()
     {
@@ -14,14 +14,25 @@ public class InputManager : MonoBehaviour
         {
             _playerControls = new PlayerControls();
 
-            _playerControls.PlayerMovement.Movement.performed += i => movementInput = i.ReadValue<Vector2>();
+            _playerControls.PlayerMovement.Movement.performed += i => _movementInput = i.ReadValue<Vector2>();
         }
 
         _playerControls.Enable();
     }
-    
+
     void OnDisable()
     {
         _playerControls.Disable();
+    }
+
+    void HandleMovementInput()
+    {
+        verticalInput = _movementInput.y;
+        horizontalInput = _movementInput.x;
+    }
+
+    public void HandleAllInputs()
+    {
+        HandleMovementInput();
     }
 }
---
+++ Assets/Scripts/PlayerLocomotion.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+using Debug = System.Diagnostics.Debug;
+
+public class PlayerLocomotion : MonoBehaviour
+{
+    public float movementSpeed = 7;
+    public float rotationSpeed = 15;
+
+    Transform _cameraObject;
+    InputManager _inputManager;
+    Vector3 _movementVelocity;
+    Rigidbody _playerRigidbody;
+
+    public void HandleAllMovement()
+    {
+        HandleMovement();
+        HandleRotation();
+    }
+
+    void Awake()
+    {
+        _inputManager = GetComponent<InputManager>();
+        _playerRigidbody = GetComponent<Rigidbody>();
+        Debug.Assert(Camera.main != null, "Camera.main != null");
+        _cameraObject = Camera.main.transform;
+    }
+
+    void HandleMovement()
+    {
+        _movementVelocity = _cameraObject.forward * _inputManager.verticalInput;
+        _movementVelocity += _cameraObject.right * _inputManager.horizontalInput;
+        _movementVelocity.Normalize();
+        _movementVelocity.y = 0;
+        _movementVelocity *= movementSpeed;
+
+        _playerRigidbody.velocity = _movementVelocity;
+    }
+
+    void HandleRotation()
+    {
+        var targetDirection = _cameraObject.forward * _inputManager.verticalInput;
+        targetDirection += _cameraObject.right * _inputManager.horizontalInput;
+        targetDirection.Normalize();
+        targetDirection.y = 0;
+
+        if (targetDirection != Vector3.zero)
+            targetDirection = transform.forward;
+
+        var targetRotation = Quaternion.LookRotation(targetDirection);
+        var playerRotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+
+        transform.rotation = playerRotation;
+    }
+}
